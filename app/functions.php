aae4e4a6d9cd39c5c1f08f56f6d1b2099799eb57
Andreawingardh <andreawingardh@gmail.com>
2024-12-18 08:32:20 +0100

    Restructured functions

--- app/functions.php
+++ app/functions.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$S_SESSION['messages'] = [];
 
 use GuzzleHttp\Exception\ClientException;
 
@@ -14,7 +13,7 @@ use GuzzleHttp\Exception\ClientException;
 function getBookingData($formData)
 {
 
-    $database = new PDO('sqlite:' . __DIR__ . '/app/database/test.db');
+    $database = new PDO('sqlite:' . __DIR__ . '/database/test.db');
     $tourist = trim(htmlspecialchars($formData['tourist']));
     $checkInDate = trim(htmlspecialchars($formData['check-in-date']));
     $checkOutDate = trim(htmlspecialchars($formData['check-out-date']));
@@ -98,9 +97,6 @@ function isValidUuid(string $uuid): bool
 //     }
 // }
 
-function getBooking($formData) {}
-
-/* This function gets the total cost of the stay to send to the API */
 
 
 /*This function checks availability of hotel rooms */
@@ -126,7 +122,6 @@ function checkAvailability($bookingData)
 
     /* $availability returns 0 if there is an availability, a higher number if the room is booked */
     $availability = $statement->fetchColumn();
-    var_dump($availability);
     return $availability;
 }
 
@@ -143,25 +138,26 @@ function getTotalCost($bookingData)
     $statement = $database->prepare("Select room_cost from hotel_rooms WHERE id = :room_id;");
     $statement->bindParam(':room_id', $roomID);
     $statement->execute();
-    $roomCost = $statement->fetchColumn(PDO::FETCH_ASSOC);
+    $roomCost = $statement->fetchColumn();
 
     /*This checks the cost of the chosen features*/
+    $totalFeatureCost = 0;
     $statement = $database->prepare("SELECT feature_cost
         from features
         where id = :feature_id;");
     foreach ($features as $feature) {
         $statement->bindParam(':feature_id', $feature);
         $statement->execute();
-        $featureCost = $statement->fetchColumn();
-        $featureCost += $featureCost;
+        $singleFeatureCost = $statement->fetchColumn();
+        $totalFeatureCost += $singleFeatureCost;
     }
-
+    echo 'Feature cost: ' . $totalFeatureCost;
 
     /* This converts the dates to UNIX time */
     $totalDays = (strtotime($checkOutDate) - strtotime($checkInDate)) / (60 * 60 * 24);
 
     /* This calculates the room stay + the cost of feature*/
-    $totalCost = $roomCost * $totalDays + $featureCost;
+    $totalCost = ($roomCost * $totalDays) + $totalFeatureCost;
     $bookingData['total-cost'] = $totalCost;
 
     /* This adds the total cost to $_SESSION['messages] */
@@ -177,51 +173,46 @@ function checkTransferCode($bookingData)
     $transferCode = $bookingData['transfer-code'];
     $totalCost = $bookingData['total-cost'];
 
-    if (isValidUuid($transferCode)) {
+    try {
+        $client = new GuzzleHttp\Client();
+        /* This checks if the transfercode and total cost match*/
+        $res = $client->request('POST', 'https://yrgopelago.se/centralbank/transferCode', [
+            'form_params' => [
+                'transferCode' => $transferCode,
+                'totalcost' => $totalCost
+            ]
+        ]);
+        /* This converts the data from the API endpoint to be accessed as an array */
+        $body = $res->getBody();
+        $stringBody = (string) $body;
+        $transferCodeResult = json_decode($stringBody, true);
+        var_dump($transferCodeResult);
+        return $transferCodeResult;
+    } catch (ClientException $e) {
+        /* If the fetch returns an error response, this code gets the contents of the response and adds it to $_SESSION['messages'] */
+        $response = $e->getResponse();
+        $errorContent = $response->getBody()->getContents();
 
-        try {
-            $client = new GuzzleHttp\Client();
-            /* This checks if the transfercode and total cost match*/
-            $res = $client->request('POST', 'https://yrgopelago.se/centralbank/transferCode', [
-                'form_params' => [
-                    'transferCode' => $transferCode,
-                    'totalcost' => $totalCost
-                ]
-            ]);
-            /* This converts the data from the API endpoint to be accessed as an array */
-            $body = $res->getBody();
-            $stringBody = (string) $body;
-            $transferCodeResult = json_decode($stringBody, true);
-            var_dump($transferCodeResult);
-            return $transferCodeResult;
-        } catch (ClientException $e) {
-            /* If the fetch returns an error response, this code gets the contents of the response and adds it to $_SESSION['messages'] */
-            $response = $e->getResponse();
-            $errorContent = $response->getBody()->getContents();
-
-            $errorMessage = json_decode($errorContent, true);
-            return $_SESSION['messages'][] = $errorMessage['error'];
-        }
-    } else {
-        return $_SESSION['messages'][] = 'Your transfer code is not valid';
+        $errorMessage = json_decode($errorContent, true);
+        return $_SESSION['messages'][] = $errorMessage['error'];
     }
 }
+
 
 /* This function deposits the transfer code */
 function depositTransferCode($transferCode)
 {
     try {
-        if (isset($transferCodeResult['status']) && $transferCodeResult['status'] = 'success') {
-            /* This sends the data to the deposit endpoint */
-            $client = new GuzzleHttp\Client();
-            $res = $client->request('POST', 'https://yrgopelago.se/centralbank/deposit', [
-                'form_params' => [
-                    'user' => 'Andy',
-                    'transferCode' => $transferCode
-                ]
-            ]);
-            $_SESSION['messages'][] = "Your booking is successful";
-        }
+
+        /* This sends the data to the deposit endpoint */
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('POST', 'https://yrgopelago.se/centralbank/deposit', [
+            'form_params' => [
+                'user' => 'Andy',
+                'transferCode' => $transferCode
+            ]
+        ]);
+        return $_SESSION['messages'][] = "Your booking is successful";
     } catch (ClientException $e) {
         /* If the fetch returns an error response, this code gets the contents of the response and adds it to $_SESSION['messages'] */
         $response = $e->getResponse();
@@ -234,7 +225,9 @@ function depositTransferCode($transferCode)
 
 
 
-
+/* This function sends the booking data to the database */
+/*@TODO: Add the value for tourist or name to the insert query. */
+/*@TODO: Figure out why the insert into booking_feature table is inserting the wrong data */
 function sendBookingData($bookingData)
 {
 
@@ -242,6 +235,7 @@ function sendBookingData($bookingData)
     $roomID = $bookingData['room-id'];
     $checkInDate = $bookingData['check-in-date'];
     $checkOutDate = $bookingData['check-out-date'];
+    $features = $bookingData['feature_id'] ?? [];
 
     /* This checks if the dates and room have been set and then inserts the data into the table */
     $statement = $database->prepare("INSERT INTO bookings (room_id, check_in_date, check_out_date) VALUES (:room_id, :check_in_date, :check_out_date);");
@@ -254,60 +248,62 @@ function sendBookingData($bookingData)
     /* This send the data to the table */
     $statement->execute();
 
+    /* This gets the last inserted ID for putting features into booking_feature table */
     $bookingID = $database->lastInsertId();
-    $bookingData['booking-id'] = $bookingID;
-    echo $bookingID;
+
+    /* This inserts features into an array with the corresponding booking_id */
+    // if (!empty($features)) {
+    //     $insertFeatures = [];
+    //     foreach ($features as $feature) {
+    //         $insertFeatures[] = [
+    //             'booking_id' => $bookingID,
+    //             'feature_id' => $feature
+    //         ];
+    //     }
+    // }
+
+    /* This prepares a statement to insert the chosen features into booking_feature table */
+    $statement = $database->prepare("INSERT INTO booking_feature (booking_id, feature_id) VALUES (:booking_id, :feature_id);");
+
+    /* This loops through the chosen features and adds them to booking_feature table */
+    foreach ($features as $feature) {
+        $statement->bindParam(':booking_id', $bookingID);
+        $statement->bindParam(':feature_id', $feature);
+        $statement->execute();
+    }
+
+
 
     /* This adds a successful messages to the $_SESSION['messages'] array */
     $_SESSION['messages'][] = "Successfully added dates to table";
     $_SESSION['messages'][] = 'You will check in on ' . $checkInDate . ' and out on ' . $checkOutDate;
-    return $bookingData['booking-id'];
 }
 
-/* This function enters features into database */
-function putFeaturesIntoDatabase($bookingData)
-{
-    $database = new PDO('sqlite:' . __DIR__ . '/database/test.db');
-
-    $features = $bookingData['feature_id'] ?? [];
-    $bookingID = $bookingData['booking_id'];
-
-    $insertFeatures = [];
-    foreach ($features as $feature) {
-        $insertFeatures[] = [
-            'booking_id' => $bookingID,
-            'feature_id' => $feature
-        ];
-    }
-
-    $statement = $database->prepare("INSERT INTO booking_feature (booking_id, feature_id) VALUES (:booking_id, :feature_id);");
-
-    foreach ($insertFeatures as $feature) {
-        $statement->bindParam(':booking_id', $feature['booking_id']);
-        $statement->bindParam(':feature_id', $feature['feature_id']);
-        $statement->execute();
-    }
-}
-
+/* This function writes the data to a json-file and sends the user a link */
+/*@TODO: Why does the file_put_contents part of this function not work */
 function createJsonReceipt($bookingData)
 {
 
     $database = $bookingData['database'];
-    $features = $bookingData['feature-id'];
+    $features = $bookingData['features'] ?? [];
+    $featuresInformation = [];
 
 
     $statement = $database->prepare("SELECT feature_item, feature_cost
         from features
         where id = :feature_id;");
-    foreach ($features as $feature) {
-        $statement->bindParam(':feature_id', $feature);
-        $statement->execute();
-        $result = $statement->fetch(PDO::FETCH_ASSOC);
-        $featuresInformation[] = [
-            'feature' => $result['feature_item'],
-            'cost' => $result['feature_cost']
-        ];
+    if (!empty($features)) {
+        foreach ($features as $feature) {
+            $statement->bindParam(':feature_id', $feature);
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            $featuresInformation[] = [
+                'feature' => $result['feature_item'],
+                'cost' => $result['feature_cost']
+            ];
+        }
     }
+
 
     $receipt = [
         [
@@ -315,9 +311,9 @@ function createJsonReceipt($bookingData)
             'hotel' => "DEFAULT HOTEL",
             "arrival_date" => $bookingData['check-in-date'],
             "departure_date" => $bookingData['check-out-date'],
-            "total_cost" => $bookingData['totalcost'],
+            "total_cost" => $bookingData['total-cost'],
             "stars" => "3",
-            $featuresInformation
+            "features" => $featuresInformation
         ],
         "additional_info" => [
             "greeting" => "Thank you for choosing DEFAULT HOTEL",
@@ -330,4 +326,23 @@ function createJsonReceipt($bookingData)
     $_SESSION['messages'][] = '
     <a href="/receipt.json" target="_blank">Your receipt</a>
     <?php ';
+    return header('Location: /');
+
+    // $jsonResult = json_encode($receipt, JSON_PRETTY_PRINT);
+
+    // if ($jsonResult === false) {
+    //     // Log JSON encoding error
+    //     $_SESSION['messages'][] = 'Error encoding JSON: ' . json_last_error_msg();
+    //     return false;
+    // }
+
+    // $writeResult = file_put_contents('receipt.json', $jsonResult);
+
+    // if ($writeResult === false) {
+    //     $_SESSION['messages'][] = 'Error writing JSON file';
+    //     return false;
+    // }
+
+    // $_SESSION['messages'][] = '<a href="/receipt.json" target="_blank">Your receipt</a>';
+    // return header('Location: /');
 }
