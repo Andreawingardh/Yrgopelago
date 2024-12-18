69d3c70c9398cf17a6ede730f7116d96f1908e59
Andreawingardh <andreawingardh@gmail.com>
2024-12-18 08:31:55 +0100

    Continued doing tests of functions

--- test.php
+++ test.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 require __DIR__ . '/app/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-$database = new PDO('sqlite:' . __DIR__ . '/app/database/test.db');
+
 
 // $statement = $database->query("SELECT * FROM bookings;");
 // $result = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -19,12 +20,31 @@ $database = new PDO('sqlite:' . __DIR__ . '/app/database/test.db');
 if (isset($_POST['submit'])) {
 
     /* This checks if the form has been submitted and applues variable names to check-in-date and check-out-date */
-    $checkInDate = $_POST['check-in-date'];
-    $checkOutDate = $_POST['check-out-date'];
-    $roomID = $_POST['room_id'];
-    $feature_id = $_POST['feature_id'];
+    $database = new PDO('sqlite:' . __DIR__ . '/app/database/test.db');
+    $checkInDate = trim(htmlspecialchars($_POST['check-in-date']));
+    $checkOutDate = trim(htmlspecialchars($_POST['check-out-date']));
+    $roomID = htmlspecialchars($_POST['room_id']);
+    $transferCode = trim(htmlspecialchars($_POST['transferCode']));
+    $features = $_POST['feature_id'] ?? [];
+    $totalCost = '';
+    $bookingID = '';
+
+    $bookingData = [
+        'database' => $database,
+        'booking-id' => $bookingID,
+        'check-in-date' => $checkInDate,
+        'check-out-date' => $checkOutDate,
+        'room-id' => $roomID,
+        'features' => $features,
+        'total-cost' => $totalCost
+    ];
+
+
+
+
+
     if (isset($checkInDate, $checkOutDate, $roomID)) {
-        
+
         /* This checks if there is availability in the room at the given dates */
         $statement = $database->prepare("SELECT COUNT(*) as availability FROM bookings
         WHERE room_id = :room_id
@@ -43,6 +63,30 @@ if (isset($_POST['submit'])) {
         /* If there is availability, then the data is added to the table */
         if ($availability == 0) {
 
+            /* This calculates the total cost of the stay */
+            /* This selects the data from the table for that specific feature_id */
+
+
+            
+
+            /* This selects the data from the table for that specific room_id */
+            $statement = $database->prepare("Select room_cost from hotel_rooms WHERE id = :room_id;");
+            $statement->bindParam(':room_id', $roomID);
+            $statement->execute();
+            $roomCost = $statement->fetchColumn(PDO::FETCH_ASSOC);
+
+
+
+            /* This converts the dates to UNIX time */
+            $totalDays = (strtotime($checkOutDate) - strtotime($checkInDate)) / (60 * 60 * 24);
+
+            /* This calculates the room stay + the cost of feature*/
+            $totalCost = $roomCost * $totalDays + $features['feature_cost'];
+
+            /* This adds the total cost to $_SESSION['messages] */
+            $_SESSION['messages'][] = 'Your total cost is $' . $totalCost . '.';
+
+
             /* This checks if the dates and room have been set and then inserts the data into the table */
             $statement = $database->prepare("INSERT INTO bookings (room_id, check_in_date, check_out_date) VALUES (:room_id, :check_in_date, :check_out_date);");
 
@@ -54,45 +98,52 @@ if (isset($_POST['submit'])) {
             /* This send the data to the table */
             $result = $statement->execute();
 
-            /* This calculates the total cost of the stay */
-            /* This selects the data from the table for that specific feature_id */
-            $statement = $database->prepare("Select * from features WHERE id = :feature_id;");
-            $statement->bindParam(':feature_id', $feature_id);
-            $statement->execute();
-            /* This selects the data from the table for that specific room_id */
-            $features = $statement->fetch(PDO::FETCH_ASSOC);
-            $statement = $database->prepare("Select * from hotel_rooms WHERE id = :room_id;");
-            $statement->bindParam(':room_id', $roomID);
-            $statement->execute();
-            $room = $statement->fetch(PDO::FETCH_ASSOC);
-
-            /* This converts the dates to UNIX time */
-            $totalDays = (strtotime($checkOutDate) - strtotime($checkInDate))/(60*60*24);
-
-            /* This calculates the room stay + the cost of feature*/
-            $totalCost = $room['room_cost'] * $totalDays + $features['feature_cost'];
-
-            /* This adds the total cost to $_SESSION['messages] */
-            $_SESSION['messages'][] = 'Your total cost is $' . $totalCost . '.';
-        
-
+            $bookingID = $database->lastInsertId();
+            echo $bookingID;
             /* This adds a successful messages to the $_SESSION['messages'] array */
             $_SESSION['messages'][] = "Successfully added dates to table";
             $_SESSION['messages'][] = 'You will check in on ' . $checkInDate . ' and out on ' . $checkOutDate;
+
+            $receipt = [
+                'island' => "DEFAULT ISLAND",
+                'hotel' => "DEFAULT HOTEL",
+                "arrival_date" => $checkInDate,
+                "departure_date" => $checkOutDate,
+                "total_cost" => $totalCost,
+                "stars" => "3",
+                "features" => [
+                    [
+                        "name" => "sauna",
+                        "cost" => 2
+                    ]
+                ],
+                "addtional_info" => [
+                    "greeting" => "Thank you for choosing DEFAULT HOTEL",
+                    "imageUrl" => "https://upload.wikimedia.org/wikipedia/commons/e/e2/Hotel_Boscolo_Exedra_Nice.jpg"
+                ]
+            ];
+
+            file_put_contents('receipt.json', json_encode($receipt));
+            
+            $_SESSION['messages'][] = '
+            <a href="/receipt.json" target="_blank">Your receipt</a>
+            <?php ';
         } else {
 
-        /* If the dates are booked, an error message is added to the $_SESSION['messages]-array */
+            /* If the dates are booked, an error message is added to the $_SESSION['messages]-array */
             $_SESSION['messages'][] = 'Sorry, these dates are booked, please try again.';
         }
+    } else {
+        $_SESSION['messages'][] = 'Please remember to fill in all the details';
     }
     var_dump($_POST);
 }
 
 
 
-    // $checkInDate = $_POST['check-in-date'];
-    // $checkOutDate = $_POST['check-out-date'];
-    // $roomID = $_POST['room_id'];
+// $checkInDate = $_POST['check-in-date'];
+// $checkOutDate = $_POST['check-out-date'];
+// $roomID = $_POST['room_id'];
 
 
 // function getTotalCost ($formData) {
@@ -149,6 +200,10 @@ if (isset($_POST['submit'])) {
             <legend>Choose your dates</legend>
             <label for="check-in-date">Check-in-date:</label><input type="date" name="check-in-date" id="check-in-date" min="2025-01-01" max="2025-01-31" required><br />
             <label for="check-out-date">Check-in-date:</label><input type="date" min="2025-01-01" max="2025-01-31" name="check-out-date" id="check-out-date" required>
+        </fieldset>
+        <fieldset>
+            <legend>Enter your transfer-code:</legend>
+            <input type="text" id="transfer-code" name="transferCode" required></input>
         </fieldset>
         <button type="submit" name="submit">Book your room!</button>
     </form>
