7363b5f74c17b159fee9e784d0e2891f30c584f2
Andreawingardh <andreawingardh@gmail.com>
2024-12-20 14:41:17 +0100

    Readded images, edited text.

--- rooms.php
+++ rooms.php
@@ -2,12 +2,12 @@
     <article>
         <div>
             <h2>No Money In The Bank, Gotta Get to Clowning Budget Singles ($1)</h2>
-            <p>We get it, you’re spending all your time clowning, but the money just isn’t rolling in like you hoped. Maybe you need to bring a friend to even afford a stay at our hotel? We’ve got just the room for you! No closets, water or otherwise is available and this room’s window faces the so-called elephant dumping ground. But at least you’re not sleeping in your own bed!</p>
+            <p>We get it, you’re spending all your time clowning, but the money just isn’t rolling in like you hoped. Maybe you need to bring a friend to even afford a stay at our hotel? We’ve got just the room for you! No closets, water or otherwise are available in this room and the window faces the so-called elephant dumping ground. But at least you’re not sleeping in your own bed!</p>
         </div>
-        <img src="assets/images/midrange clown hotel room with two single beds 1.png">
+        <img src="assets/images/budget-hotel-room.jpg">
     </article>
     <article>
-        <img src="assets/images/a budget clown hotel room without a clown in it 1.png">
+        <img src="assets/images/standard-hotel-room.jpg">
         <div>
 
             <h2>Clown All Day Sleep All Night Superior Double ($2)</h2>
@@ -19,6 +19,6 @@
             <h2>Ultra Deluxe Manic Circus Suite ($3)</h2>
             <p>Have you got more clown bucks than you know how to spend? We’ll tell you how to spend it! Book a couple of nights in our Ultra Deluxe Manic Circus Suite! Not only does the room have a toilet in the kitchen, an oven in the bathroom and a whole goshdarn circus in the closet, sleeping on the Ultra Deluxe Manic Suite bed is like sleeping on a cloud of balloons. You won’t even be able to guess where you’ll wake up in the morning! That’s a Hotel de Pierrot promise! Just fork over the cash and you’ve got this magical clown heaven all to yourself!</p>
         </div>
-        <img src="assets/images/a deluxe suite for clowns 1.png">
+        <img src="assets/images/luxury-hotel-room.jpg">
     </article>
 </section>
