243fd109d2ff09bc590d6f37ede2f5da0eff56a7
Andreawingardh <andreawingardh@gmail.com>
2024-12-20 10:35:10 +0100

    Edited markup of calendar

--- calendar.php
+++ calendar.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 ?>
 
+
+
+<section id="pre-booking">
+
 <section id="calendar">
-    <h1>Check out when our rooms are booked</h1>
-    <button id="budget-button">Budget</button> <button id="standard-button">Standard</button><button id="luxury-button">Luxury</button>
-    <div id="budget-room"><?php getCalendar("1"); ?></div>
-    <div id="standard-room"><?php getCalendar("2"); ?></div>
-    <div id="luxury-room"><?php getCalendar("3"); ?></div>
+    <h2>Ready to book?</h2>
+    <p>Check out out when our rooms are free!</p>
+    <div class="display-buttons"><button id="budget-button">Budget</button> <button id="standard-button">Standard</button><button id="luxury-button">Luxury</button></div>
+    <div id="budget-room"><h2>Budget</h2><?php getCalendar(1); ?></div>
+    <div id="standard-room"><h2>Standard</h2> <?php getCalendar(2); ?></div>
+    <div id="luxury-room"><h2>Luxury</h2> <?php getCalendar(3); ?></div>
     <script src="/assets/scripts/script.js"></script>
+</section>
 </section>
