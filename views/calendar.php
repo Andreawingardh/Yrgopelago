6d1a440d06b46675665393eb4f82bf8e3b781459
Andreawingardh <andreawingardh@gmail.com>
2025-01-03 12:06:07 +0100

    Renamed buttons in calendar

--- views/calendar.php
+++ views/calendar.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
 <section id="calendar">
     <h2>Ready to book?</h2>
     <p>Check out out when our rooms are free!</p>
-    <div class="display-buttons"><button id="budget-button">Budget</button> <button id="standard-button">Standard</button><button id="luxury-button">Luxury</button></div>
+    <div class="display-buttons"><button id="budget-calendar-button">Budget</button> <button id="standard-calendar-button">Standard</button><button id="luxury-calendar-button">Luxury</button></div>
     <div id="budget-room"><h2>Budget</h2><?php getCalendar(1); ?></div>
     <div id="standard-room"><h2>Standard</h2> <?php getCalendar(2); ?></div>
     <div id="luxury-room"><h2>Luxury</h2> <?php getCalendar(3); ?></div>
