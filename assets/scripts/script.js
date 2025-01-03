5d373eb255c0d3d48fa5323a04de885b91c14f0c
Andreawingardh <andreawingardh@gmail.com>
2025-01-03 12:05:46 +0100

    Created javascript functions for buttons from rooms to display calendar and fill in correct field in form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -94,9 +94,16 @@ function showCalendar() {
     standardRoom.style.display = 'none';
     luxuryRoom.style.display = 'none';
 
+    //Create buttons in room view
     const budgetButton = document.getElementById("budget-button");
     const standardButton = document.getElementById("standard-button");
     const luxuryButton = document.getElementById("luxury-button");
+
+
+    // Create buttons in calendar
+    const budgetCalendarButton = document.getElementById("budget-calendar-button");
+    const standardCalendarButton = document.getElementById("standard-calendar-button");
+    const luxuryCalendarButton = document.getElementById("luxury-calendar-button");
 
     // Function to hide all rooms
     function hideAllCalendars() {
@@ -105,22 +112,88 @@ function showCalendar() {
         luxuryRoom.style.display = 'none';
     }
 
-    // Add click event listeners to buttons
+    // Add click event listeners to buttons for calendar
+    budgetCalendarButton.addEventListener('click', () => {
+        hideAllCalendars();
+        budgetRoom.style.display = 'block';
+    });
+
+    standardCalendarButton.addEventListener('click', () => {
+        hideAllCalendars();
+        standardRoom.style.display = 'block';
+    });
+
+    luxuryCalendarButton.addEventListener('click', () => {
+        hideAllCalendars();
+        luxuryRoom.style.display = 'block';
+    });
+
+
+    // Add click event listeners to buttons from room
     budgetButton.addEventListener('click', () => {
         hideAllCalendars();
+        document.getElementById("calendar").scrollIntoView({behavior: 'smooth'});
         budgetRoom.style.display = 'block';
     });
 
     standardButton.addEventListener('click', () => {
         hideAllCalendars();
+        document.getElementById("calendar").scrollIntoView({behavior: 'smooth'});
         standardRoom.style.display = 'block';
     });
 
     luxuryButton.addEventListener('click', () => {
         hideAllCalendars();
+        document.getElementById("calendar").scrollIntoView({behavior: 'smooth'});
         luxuryRoom.style.display = 'block';
     });
+
 
 }
 
 showCalendar();
+
+function moveToBooking() {
+    const roomSelect = document.getElementById('hotel-rooms');
+    const budgetRoom = document.getElementById("budget-room");
+    const standardRoom = document.getElementById("standard-room");
+    const luxuryRoom = document.getElementById("luxury-room");
+
+    function hideAllCalendars() {
+        budgetRoom.style.display = 'none';
+        standardRoom.style.display = 'none';
+        luxuryRoom.style.display = 'none';
+    }
+    const budgetBookingButton = document.getElementById("budget-booking-button");
+    budgetBookingButton.addEventListener('click', () => {
+        roomSelect.value = '1';
+        roomSelect.dispatchEvent(new Event('change'));
+        document.getElementById("hotel-booking").scrollIntoView({behavior: 'smooth'});
+        hideAllCalendars();
+        budgetRoom.style.display = 'block';
+        
+    })
+
+    const standardBookingButton = document.getElementById("standard-booking-button");
+    standardBookingButton.addEventListener('click', () => {
+        roomSelect.value = '2';
+        roomSelect.dispatchEvent(new Event('change'));
+        document.getElementById("hotel-booking").scrollIntoView({behavior: 'smooth'});
+        hideAllCalendars();
+        standardRoom.style.display = 'block';
+        
+    })
+
+    const luxuryBookingButton = document.getElementById("luxury-booking-button");
+    luxuryBookingButton.addEventListener('click', () => {
+        roomSelect.value = '3';
+        roomSelect.dispatchEvent(new Event('change'));
+        document.getElementById("hotel-booking").scrollIntoView({behavior: 'smooth'});
+        hideAllCalendars();
+        luxuryRoom.style.display = 'block';
+        
+    })
+}
+
+
+moveToBooking();    
