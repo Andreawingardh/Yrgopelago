c2621ee547812cad7ef1bc32e42ba8f27604f879
Andreawingardh <andreawingardh@gmail.com>
2025-01-03 12:06:15 +0100

    Created anchor link

--- views/form.php
+++ views/form.php
@@ -67,7 +67,7 @@ declare(strict_types=1);
 
     if (isset($_SESSION['messages']['receipt'])) {
     ?>
-        <h2>Your booking is successful! Welcome to Hotel de Pierrot. We hope you enjoy your stay!</h2>
+        <h2 id="successful-booking">Your booking is successful! Welcome to Hotel de Pierrot. We hope you enjoy your stay!</h2>
         <a href="<?= $_SESSION['messages']['receipt'] ?>" target="_blank">Your receipt</a>
         <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTV2cW8xOHJyZTgzdXVocTZmbXdsM3NyNXplNDVxcWd5azhvNWNzNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LD7LJhWI2u1lqf5oUD/giphy.gif" />
     <?php
