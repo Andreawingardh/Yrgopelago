cd9a730bf07bb71eb21e3fbc31a7602b1e669039
Andreawingardh <andreawingardh@gmail.com>
2025-01-07 11:37:15 +0100

    Edited code for atm.php to clear after button submitted

--- views/atm.php
+++ views/atm.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 
 
-
-
 ?>
 <section id="atm">
     <h2>Want to make a withdrawal?</h2>
     <p>Here at Hotel de Pierrot we make it easy for you to get the money you need to book a room. The money is only a click away!</p>
-    <form id="withdraw-form" action="/#atm" method="post">
+    <form id="withdraw-form" action="/Yrgopelago/index.php#atm" method="post">
         <fieldset>
             <legend>Name:</legend>
             <input type="text" name="user" label="user"></input>
@@ -31,12 +29,14 @@ declare(strict_types=1);
         if (isset($withdrawResult['transferCode'])) {
     ?>
             <p class="withdrawal">Your transfercode: <?= $withdrawResult['transferCode']; ?><br>
-            Your amount: <?= $withdrawResult['amount']; ?></p>
-    <?php } else if (!isset($withdrawResult['transferCode'])) {
+                Your amount: <?= $withdrawResult['amount']; ?></p>
+    <?php
+            unset($withdrawResult);
+            $_POST = [];
+        } else if (!isset($withdrawResult['transferCode'])) {
             foreach ($_SESSION['messages'] as $error) {
                 echo 'Error: ' . $error . ' Please try again.';
             }
-            unset($_POST['atm-submit']);
         }
     }
     ?>
