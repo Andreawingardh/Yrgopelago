63b4005c0b1c69550d30b318bdc983067be72cab
Andreawingardh <andreawingardh@gmail.com>
2025-01-07 15:31:16 +0100

    Edited names for room in calendar

--- views/calendar.php
+++ views/calendar.php
@@ -8,13 +8,19 @@ declare(strict_types=1);
 
 <section id="pre-booking">
 
-<section id="calendar">
-    <h2>Ready to book?</h2>
-    <p>Check out out when our rooms are free!</p>
-    <div class="display-buttons"><button id="budget-calendar-button">Budget</button> <button id="standard-calendar-button">Standard</button><button id="luxury-calendar-button">Luxury</button></div>
-    <div id="budget-room"><h2>Budget</h2><?php getCalendar(1); ?></div>
-    <div id="standard-room"><h2>Standard</h2> <?php getCalendar(2); ?></div>
-    <div id="luxury-room"><h2>Luxury</h2> <?php getCalendar(3); ?></div>
-    <script src="/assets/scripts/script.js"></script>
-</section>
+    <section id="calendar">
+        <h2>Calendar</h2>
+        <p>Check out out when our rooms are free!</p>
+        <div class="display-buttons"><button id="budget-calendar-button">Budget Single</button> <button id="standard-calendar-button">Superior Double</button><button id="luxury-calendar-button">Circus Suite</button></div>
+        <div id="budget-room">
+            <h2>Budget Single</h2><?php getCalendar(1); ?>
+        </div>
+        <div id="standard-room">
+            <h2>Superior Double</h2> <?php getCalendar(2); ?>
+        </div>
+        <div id="luxury-room">
+            <h2>Circus Suite</h2> <?php getCalendar(3); ?>
+        </div>
+        <script src="/assets/scripts/script.js"></script>
+    </section>
 </section>
