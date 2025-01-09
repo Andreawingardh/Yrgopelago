18710cc847e2548e4061ece5753b4ae54ae75cfc
Andreawingardh <andreawingardh@gmail.com>
2025-01-09 17:32:36 +0100

    Cleaned up code, renamed buttons

--- views/calendar.php
+++ views/calendar.php
@@ -1,25 +1,15 @@
-<?php
-
-declare(strict_types=1);
-
-?>
-
-
-
-
 
     <section id="calendar">
         <h2>Calendar</h2>
         <p>Check out out when our rooms are free!</p>
         <div class="display-buttons"><button id="budget-calendar-button">Budget Single</button> <button id="standard-calendar-button">Superior Double</button><button id="luxury-calendar-button">Circus Suite</button></div>
-        <div id="budget-room">
+        <div id="budget-calendar">
             <h2>Budget Single</h2><?php getCalendar(1); ?>
         </div>
-        <div id="standard-room">
+        <div id="standard-calendar">
             <h2>Superior Double</h2> <?php getCalendar(2); ?>
         </div>
-        <div id="luxury-room">
+        <div id="luxury-calendar">
             <h2>Circus Suite</h2> <?php getCalendar(3); ?>
         </div>
-        <script src="/assets/scripts/script.js"></script>
     </section>
