d124bf1f05f659c4c02443567dd0b0090092afa5
Andreawingardh <andreawingardh@gmail.com>
2025-01-09 17:33:41 +0100

    Renamed buttons

--- views/rooms.php
+++ views/rooms.php
@@ -3,7 +3,7 @@
         <div>
             <h2>No Money In The Bank, Gotta Get to Clowning Budget Single ($1)</h2>
             <p>We get it, you’re spending all your time clowning, but the money just isn’t rolling in like you hoped. Maybe you need to bring a friend to even afford a stay at our hotel? We’ve got just the room for you! No closets, water or otherwise are available in this room and the window faces the so-called elephant dumping ground. But at least you’re not sleeping in your own bed!</p>
-            <div class="room-buttons"><button id="budget-button">Check dates</button><button id="budget-booking-button">Book room</button></div>
+            <div class="room-buttons"><button id="budget-check-date-button">Check dates</button><button id="budget-book-now-button">Book room</button></div>
         </div>
         <img src="assets/images/budget-hotel-room.jpg">
     </article>
@@ -13,15 +13,16 @@
 
             <h2>Clown All Day Sleep All Night Superior Double ($2)</h2>
             <p>Maybe you want to bring your significant clown other on a fantastical getaway? Look no further! A cozy double with just enough whimsy to satisfy your inner and outer clown, our Clown All Day, Sleep All Night Superior Double is just the room for you! Breakfast included (that means the popcorn machine!)</p>
-            <div class="room-buttons"><button id="standard-button">Check dates<button id="standard-booking-button">Book room</button</div>
+            <div class="room-buttons"><button id="standard-check-date-button">Check dates<button id="standard-book-now-button">Book room</button</div>
         </div>
     </article>
     <article>
         <div>
             <h2>Ultra Deluxe Manic Circus Suite ($3)</h2>
             <p>Have you got more clown bucks than you know how to spend? We’ll tell you how to spend it! Book a couple of nights in our Ultra Deluxe Manic Circus Suite! Not only does the room have a toilet in the kitchen, an oven in the bathroom and a whole goshdarn circus in the closet, sleeping on the Ultra Deluxe Manic Suite bed is like sleeping on a cloud of balloons. You won’t even be able to guess where you’ll wake up in the morning! That’s a Hotel de Pierrot promise! Just fork over the cash and you’ve got this magical clown heaven all to yourself!</p>
-            <div class="room-buttons"><button id="luxury-button">Check dates</button><button id="luxury-booking-button">Book room</button></div>
+            <div class="room-buttons"><button id="luxury-check-date-button">Check dates</button><button id="luxury-book-now-button">Book room</button></div>
         </div>
         <img src="assets/images/luxury-hotel-room.jpg">
     </article>
 </section>
+<hr>
